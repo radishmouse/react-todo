1d79120b555b414656f18bf3dd808aeb6d017ab3
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-03 15:17:05 -0500

    adds _onSubmit and _onChange function to TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import TodoList from "./TodoList";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
   render() {
     return (
       <div>
         <h1>Todo App</h1>
+        <form onSubmit={this._onSubmit}>
+          <input
+            value={this.state.term}
+            onChange={this._onChange}
+            placeholder="Enter Todo"
+          />
+          <button type="submit">add</button>
+        </form>
       </div>
     );
   }
+
+  _onChange = event => {
+    this.setState({
+      term: event.target.value
+    });
+  };
+
+  _onSubmit = event => {
+    //puts on the brakes
+    event.preventDefault();
+    // console.log("submitted!");
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  };
 }
 
 export default TodoList;
